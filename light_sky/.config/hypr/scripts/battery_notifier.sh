2408b685328759992b84fb94100a83fa6e4c1b4e
NathanGros <nathan.gros6@gmail.com>
2025-10-20 16:11:31 +0200

    modified battery notification script

--- light_sky/.config/hypr/scripts/battery_notifier.sh
+++ light_sky/.config/hypr/scripts/battery_notifier.sh
@@ -5,7 +5,7 @@ while true; do
   is_charging=$(cat /sys/class/power_supply/ACAD/online)
   if [ "$bat_lvl" -le 20 ]; then
     if [ "$is_charging" -eq 0 ] && [ $is_notification_on -eq 0 ]; then
-      notify-send --urgency=CRITICAL "Battery Low" "Level: ${bat_lvl}%\nClick to dismiss"
+      notify-send --urgency=CRITICAL -a "Battery low" "${bat_lvl}% remaining" "Click to dismiss"
       is_notification_on=1
     fi
     if [ "$is_charging" -eq 1 ] && [ $is_notification_on -eq 1 ]; then
