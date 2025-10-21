b7adc2e2cc87c23366f110793e097bdd0708a52c
NathanGros <nathan.gros6@gmail.com>
2025-10-21 17:13:32 +0200

    small changes

--- light_sky/.config/hypr/scripts/updates_count.sh
+++ light_sky/.config/hypr/scripts/updates_count.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
-# -*- coding: utf-8 -*-
-aur_helper="yay"
-count=$($aur_helper -Qu 2>/dev/null | wc -l)
-char="•"
 
-if (( count == 0 )); then
-  printf "0\n"
-elif (( count < 11 )); then
-  for ((i=0; i<count-1; i++)); do
+# Get update updates_count
+if ! updates_arch="$(checkupdates | wc -l)"; then
+    updates_arch=0
+fi
+if ! updates_aur="$(yay -Qum 2>/dev/null | wc -l)"; then
+    updates_aur=0
+fi
+updates_count="$((updates_arch + updates_aur))"
+
+# Format output string
+char="•"
+if (( updates_count == 0 )); then
+    printf "0\n"
+elif (( updates_count < 11 )); then
+    for ((i=0; i<updates_count-1; i++)); do
+        printf "%s" "$char"
+        printf " "
+    done
     printf "%s" "$char"
-    printf " "
-  done
-  printf "%s" "$char"
-  printf "\n"
-elif (( count < 20 )); then
-  for ((i=0; i<count; i++)); do
-    printf "%s" "$char"
-  done
-  printf "\n"
+    printf "\n"
+elif (( updates_count < 20 )); then
+    for ((i=0; i<updates_count; i++)); do
+        printf "%s" "$char"
+    done
+    printf "\n"
 else
-  printf "• • • • •  %s\n" "$count"
+    printf "• • • • •  %s\n" "$updates_count"
 fi
