f928ac22aa5498a7e0deb60e0ec3c12883fb6371
Nathan Gros <nathan.gros6@gmail.com>
2025-12-10 13:29:05 +0100

    modified color-theme config for neovim

--- light_sky/.config/nvim/lua/plugins/toggle-theme.lua
+++ light_sky/.config/nvim/lua/plugins/toggle-theme.lua
@@ -2,9 +2,9 @@ return {
 	dir = vim.fn.stdpath("config") .. "/lua/myplugins/toggle-theme.nvim",
 	dependencies = {
 		"catppuccin/nvim",
-        "nvim-lualine/lualine.nvim",
+		"nvim-lualine/lualine.nvim",
 	},
-    config = function ()
-        require("toggle-theme").setup()
-    end
+	config = function ()
+		require("toggle-theme").setup("beige_theme")
+	end
 }
